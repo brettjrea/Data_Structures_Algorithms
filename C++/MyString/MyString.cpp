64c2a23e11cf33a48984a1baacdcf746920b7369
brettjrea <6890798+brettjrea@users.noreply.github.com>
2022-09-10 00:34:36 +0000

    push

---
+++ C++/MyString/MyString.cpp
@@ -0,0 +1,203 @@
+#include <iostream>
+#include<string>
+using namespace std;
+class MyString {
+private:
+	char* myString;
+	int curr_length;
+	int arr_size;
+	//Ensure capacity to array
+	void ensureCapacity() {
+		arr_size += arr_size;
+		char* temp;
+		temp = new char[curr_length + 1];
+		for (int i = 0; i < curr_length; i++) {
+			temp[i] = myString[i];
+		}
+		temp[curr_length] = '\0';
+		myString = new char[arr_size];
+		for (int i = 0; i < curr_length; i++) {
+			myString[i] = temp[i];
+		}
+		myString[curr_length] = '\0';
+	}
+public:
+	//Default constructor
+	MyString() {
+		curr_length = 0;
+		arr_size = 0;
+		myString = NULL;
+	}
+	//Parameterized constructor
+	MyString(string str) {
+		arr_size = str.length() + 1;
+		myString = new char[arr_size];
+		curr_length = 0;
+		for (int i = 0; i < str.length(); i++) {
+			myString[i] = str[i];
+			curr_length++;
+		}
+		myString[curr_length] = '\0';
+	}
+	//Copy constructor
+	MyString(const MyString& str) {
+		arr_size = str.arr_size;
+		myString = new char[arr_size];
+		curr_length = 0;
+		for (int i = 0; i < str.curr_length; i++) {
+			myString[i] = str.myString[i];
+			curr_length++;
+		}
+		myString[curr_length] = '\0';
+	}
+	//Return length of the string
+	int getLength() {
+		return curr_length;
+	}
+	//Extraction operator overload
+	friend ostream& operator<<(ostream& out, const MyString& str) {
+		for (int i = 0; i < str.curr_length; i++) {
+			out << str.myString[i];
+		}
+		return out;
+	}
+	//Concatenation
+	MyString& operator+(const MyString& str) {
+		int capacity = str.arr_size + this->arr_size;
+		while (this->arr_size < capacity) {
+			this->ensureCapacity();
+		}
+		for (int i = 0; i < str.curr_length; i++) {
+			this->myString[curr_length++] = str.myString[i];
+		}
+		this->myString[curr_length] = '\0';
+		return *this;
+	}
+	bool operator==(const MyString& str) {
+		if (str.curr_length == curr_length) {
+			for (int i = 0; i < str.curr_length; i++) {
+				if (this->myString[i] != str.myString[i]) {
+					return 0;
+				}
+			}
+			return true;
+		}
+		return false;
+	}
+	//Compare two strings
+	int compareTo(const MyString& str) {
+		//Comparing current object myString(str1) and str,Mystring(str2), Example : if str1 is less than str1 then return -1
+		if ((this->myString, str.myString) < 0) {
+			return -1;
+		}
+		 // or if str1 is greater than str2 then return 1,
+		else if ((this->myString, str.myString) > 0) {
+			return 1;
+		}
+		// if both are equal then return 0.
+		else 
+		{
+			return 0;
+		}
+	}
+	//method that takes an integer and returns the character at that index location.
+	char get(int index) {
+		if (index < curr_length) {
+			return myString[index];
+		}
+		return ' ';
+	}
+	//Uppercase a string
+	void toUpper() {
+		for (int i = 0; i < curr_length; i++) {
+			myString[i] = toupper(myString[i]);
+		}
+	}
+	//Lowercase a string
+	void toLower() {
+		for (int i = 0; i < curr_length; i++) {
+			myString[i] = tolower(myString[i]);
+		}
+	}
+	//that takes an integer and returns the substring starting at that index.
+	char* substring(int index) {
+		char * temp;
+		if (index < curr_length) {
+			temp = new char[curr_length + 1];
+			int i = 0, j = 0;
+			for (i = index; i < curr_length; i++) {
+				temp[j++] = myString[i];
+			}
+			temp[j] = '\0';
+		}
+		return temp;;
+	}
+	//Return a MyString substring where n is the starting index and m is one past the ending index.
+	char* substring(int n, int m) {
+		char * temp;
+		temp = new char[curr_length + 1];
+		if (m < curr_length) {
+			int i = 0, j = 0;
+			for (i = n; i <= m; i++) {
+				temp[j++] = myString[i];
+			}
+			temp[j] = '\0';
+		}
+		return temp;;
+	}
+	int indexOf(const MyString& str) {
+		char* temp;
+		for (int i = 0; i < curr_length; i++) {
+			if (str.myString[0] == myString[i]) {
+				temp = this->substring(i, i + str.curr_length - 1);
+				cout << temp << endl;
+				if ((str.myString, temp) == 0) {
+					return i;
+				}
+			}
+		}
+		return -1;
+	}
+	int lastIndexOf(const MyString& str) {
+		char* temp;
+		for (int i = curr_length - 1; i >= 0; i--) {
+			if (str.myString[0] == myString[i]) {
+				temp = this->substring(i, i + str.curr_length - 1);
+				if ((str.myString, temp) == 0) {
+					return i;
+				}
+			}
+		}
+		return -1;
+	}
+};
+
+int main()
+{
+	//Create 2 objects
+	MyString str1("Hello world!");
+	MyString str2("Good Morning!!");
+	//Display strings
+	cout << "String1: " << str1 << endl;
+	cout << "String2: " << str2 << endl;
+	//Concatenate and display
+	str1 = str1 + str2;
+	cout << "Concatenate 1 & 2 : " << str1 << endl;
+	//Assignment test
+	MyString str3 = str1;
+	cout << "Equal operator overload: " << str3 << endl;
+	//Comparison test
+	cout << "Comparison equal test1: " << str3.compareTo(str1) << endl;
+	cout << "Comparison equal test2: " << str3.compareTo(str2) << endl;
+	//Case changing test
+	str2.toUpper();
+	cout << "To uppercase test: " << str2 << endl;
+	str1.toLower();
+	cout << "To lowercase test: " << str2 << endl;
+	//Index test
+	str3.toUpper();
+	cout << "First index test: " << str3.indexOf(str2) << endl;
+	cout << "Last index test: " << str3.lastIndexOf(str2) << endl;
+
+	return 0;
+}
---
+++ Java/BinarySearch/BinarySearchFile.java
@@ -0,0 +1,132 @@
+package BinarySearch;
+// BinarySearchFile.java : Java program to read words from file and search strings using binary search
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class BinarySearchFile {
+
+    // method to sort the array of Strings of size in ascending order using
+    // selection sort
+    public static void sort(String[] words, int size) {
+        int min;
+        // loop over the array from index 0 to second last index
+        for (int i = 0; i < size - 1; i++) {
+            min = i;
+            // loop to get the ith minimum string from index i+1 to end
+            for (int j = i + 1; j < size; j++) {
+                if (words[j].compareTo(words[min]) < 0)
+                    min = j;
+            }
+
+            if (min != i) // index i and min are not equal, swap the strings at index i and min
+            {
+                String temp = words[i];
+                words[i] = words[min];
+                words[min] = temp;
+            }
+        }
+    }
+
+    public static void bubbleSort(String[] words, int size) {
+        String temp;
+
+        // Sorting strings using bubble sort
+        for (int j = 0; j < size - 1; j++) {
+            for (int i = j + 1; i < size; i++) {
+                if (words[j].compareTo(words[i]) > 0) {
+                    temp = words[j];
+                    words[j] = words[i];
+                    words[i] = temp;
+                }
+            }
+        }
+    }
+
+    // method to search for a string key in array words using binary search
+    public static int binarySearch(String[] words, String key, int size) {
+        int low = 0, high = size - 1; // initialize low and high to first and last index of array words
+        int mid;
+        // loop that continues until range is invalid or key is found
+        while (low <= high) {
+            mid = (low + high) / 2; // get the middle index of range
+            if (words[mid].equals(key)) // key found
+                return mid;
+            else if (words[mid].compareTo(key) > 0) // string at mid > key, find the key in left subarray
+                high = mid - 1;
+            else // string at mid < key, find the key in right subarray
+                low = mid + 1;
+        }
+
+        return -1; // key not found
+    }
+
+    public static void main(String[] args) {
+
+        // create an array of 10,000 strings
+        String[] words = new String[10000];
+        int nWords = 0;
+        String word;
+        File file = new File("Java/BinarySearch/text.txt"); // create a File object
+
+        try {
+            // open the input file
+            Scanner fileScan = new Scanner(file);
+            // loop that continues until end of file is reached or 10,000 words have been
+            // read
+            while (fileScan.hasNext() && nWords < words.length) {
+                word = fileScan.next(); // read the next word from file
+                if (word.length() > 0) // word is not an empty string
+                {
+                    words[nWords] = word; // store it at index nWords
+                    nWords++; // increment number of words
+                }
+            }
+            fileScan.close(); // close the file
+
+            /* Display unsorted array. */
+            System.out.println("Unsorted list:");
+            System.out.println(" ");
+            for (int i = 0; i < nWords; i++) {
+                System.out.print(words[i] + " ");
+            }
+            System.out.println();
+            System.out.println(" ");
+
+            /* sort the words in ascending order */
+            // sort(words, nWords);
+
+            bubbleSort(words, nWords);
+
+            /* Display sorted array. */
+            System.out.println("Sorted list:");
+            System.out.println(" ");
+            for (int i = 0; i < nWords; i++) {
+                System.out.print(words[i] + " ");
+            }
+            System.out.println();
+            System.out.println(" ");
+
+            Scanner scan = new Scanner(System.in);
+
+            // loop that continues to search for string until the user exits
+            do {
+                System.out.print("Enter a word to search(0 to exit): ");
+                word = scan.next(); // input a word to search
+                if (!word.equals("0")) // check if user wants to exit
+                {
+                    int index = binarySearch(words, word, nWords); // get the index of word using binarySearch method
+                    if (index == -1) // word not found
+                        System.out.println(word + " not found.");
+                    else // word found
+                        System.out.println(word + " found at index: " + index);
+                }
+            } while (!word.equals("0"));
+            scan.close(); // close the file
+
+        } catch (FileNotFoundException e) {
+            System.err.println("Unable to open file: text.txt");
+        }
+    }
+}
