137497798cd589f3c2bd670d2d562e197b2fd80a
brettjrea <6890798+brettjrea@users.noreply.github.com>
2022-09-22 01:04:26 +0000

    push

---
+++ Java/Deque/Main.java
@@ -0,0 +1,47 @@
+package Deque;
+import java.util.*; 
+public class Main { 
+    public static void main(String[] args) { 
+        //Declare Deque object using Array or LinkedList class to use the functionality of deque interface.
+        Deque<String> deque = new ArrayDeque<String>(); 
+        //Deque<String> deque = new LinkedList<String>(); 
+        // Common methods 
+        deque.add("One");           //add ()
+        deque.addFirst("Two");      //addFirst ()
+        deque.addLast("Three");     //addLast ()
+        deque.push("Four");         //push ()
+        deque.offer("Five");        //offer ()
+        deque.offerFirst("Six");    //offerFirst ()
+        deque.offerLast("Seven");   //offerLast ()
+        // Print details
+        System.out.println("\nDeque:");
+        System.out.println(deque + " "); 
+      
+       // Iterate using standard iterator
+        System.out.println("\nForward Iterator:"); 
+        Iterator iterator = deque.iterator(); 
+        while (iterator.hasNext()) 
+            System.out.print(" " + iterator.next()); 
+        
+       // Iterate using Reverse order iterator 
+        Iterator reverse = deque.descendingIterator(); 
+        System.out.println("\n\nReverse Iterator:"); 
+        while (reverse.hasNext()) 
+            System.out.print(" " + reverse.next()); 
+          
+        // Peek () method
+        System.out.println("\n\nPeek:" + deque.peek()); 
+        System.out.println("After peek:" + deque); 
+        
+         // Pop () method 
+        System.out.println("\nPop:" + deque.pop()); 
+        System.out.println("After pop:" + deque); 
+   
+        // contains () method
+        System.out.println("\nDeque Contains Three: " +  deque.contains("Three")); 
+   
+        deque.removeFirst();        //removeFirst () 
+        deque.removeLast();         //removeLast ()
+        System.out.println("\nRemoved first and last elements: " + deque); 
+   } 
+}
