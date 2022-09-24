54e4134f967eb2007d85d20aef5306ecc9f75d3d
brettjrea <6890798+brettjrea@users.noreply.github.com>
2022-09-24 03:44:30 +0000

    push

--- Java/PriorityQueue/Main.java
+++ Java/PriorityQueue/Main.java
@@ -1,3 +1,6 @@
+//PriorityQueue does not allow null values.
+//For non-comparable objects, we cannot create a priority queue.
+//The head or front of the queue contains the least element as per the natural ordering.
 import java.util.*; 
    
 class Main { 
@@ -14,18 +17,18 @@ class Main {
         numQueue.add("Eleven");
         numQueue.add("Nine"); 
    
-        // Print the head element using Peek () method 
-        System.out.println("Peek at head: "  + numQueue.peek()); 
-   
         // Printing all elements 
         System.out.print("\nThe PriorityQueue elements: "); 
         Iterator<?> iter1 = numQueue.iterator(); 
         while (iter1.hasNext()) 
             System.out.print(iter1.next() + " "); 
+
+        // Print the head element using Peek () method 
+        System.out.println("\n\nPeek at head: "  + numQueue.peek()); 
    
-        // remove head with poll () 
-        numQueue.poll(); 
-        System.out.print("\n\nRemoved head element with poll function: "); 
+        // return the head and remove the head with poll () 
+        
+        System.out.print("\nRemoved " + numQueue.poll() + " the head element with poll function: "); 
         Iterator<String> iter2 = numQueue.iterator(); 
         while (iter2.hasNext()) 
             System.out.print(iter2.next() + " "); 
@@ -40,11 +43,11 @@ class Main {
    
         // Check for five using contains() return true or false.
         boolean ret_val = numQueue.contains("Five"); 
-        System.out.println("\n\nUse Contain to check PriorityQueue for Five return true or false: " + ret_val); 
+        System.out.println("\n\nContains a five: " + ret_val); 
    
         // cast to array with toArray () 
         Object[] numArr = numQueue.toArray(); 
-        System.out.println("\nArray Contents: "); 
+        System.out.print("\nArray Contents: "); 
         for (int i = 0; i < numArr.length; i++) 
             System.out.print(numArr[i].toString() + " "); 
     } 
---
+++ Java/PriorityQueue/readme.txt
@@ -0,0 +1,3 @@
+
+
+https://www.loom.com/share/3e69dc0ca554422da90789c65ba24ce7
