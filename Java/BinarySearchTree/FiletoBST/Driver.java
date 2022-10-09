8388e510d775db383ffefca623d3a30c9143e1e9
brettjrea <6890798+brettjrea@users.noreply.github.com>
2022-10-09 01:12:31 +0000

    push

---
+++ Java/BinarySearchTree/FiletoBST/Driver.java
@@ -0,0 +1,66 @@
+package BinarySearchTree.FiletoBST;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Driver {
+
+	public static void main(String[] args) throws IOException {
+
+		BST bSTree = new BST();
+		Scrub Scrub = new Scrub();
+
+		ArrayList<String> inputArray = Scrub.splitString("Java/BinarySearchTree/FiletoBST/input.in");
+		System.out.println(inputArray.toString());
+
+		for (int i = 0; i <= inputArray.size() - 1; i++) {
+
+			bSTree.addNode(inputArray.get(i));
+
+		}
+		//scanner for user input
+		bSTree.inOrderTraverseTree(bSTree.root);
+		try (Scanner scanner = new Scanner(System.in)) {
+
+			do {
+
+				System.out.println("Enter string, enter -1 to quit: ");
+				String input = scanner.nextLine();
+				if (input.equals("-1")) {
+
+					break;
+				}
+
+				bSTree.findNode(input);
+			} while (true);
+
+			System.out.println("Enter string to remove: ");
+
+			do {
+
+				System.out.println("Enter string to remove, enter -1 to quit: ");
+				String removedString = scanner.nextLine();
+				if (removedString.equals("-1")) {
+
+					break;
+
+				}
+
+				boolean isRemoved = bSTree.remove(removedString);
+				if (isRemoved) {
+
+					bSTree.inOrderTraverseTree(bSTree.root);
+				} else {
+
+					System.out.println(removedString + " not found, nothing removed.");
+					bSTree.inOrderTraverseTree(bSTree.root);
+
+				}
+
+			} while (true);
+		}
+
+	}
+}
